fdef1ffc8f3deaa8a587fbefdcce3bf63e15ed88
Robson Pires <robsonvpires@gmail.com>
2023-04-13 16:46:25 -0300

    add controllers

--- MovieRentalStore.API/Controllers/MovieController.cs
+++ MovieRentalStore.API/Controllers/MovieController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRentalStore.API.DTOs;
+using Swashbuckle.AspNetCore.Annotations;
 
-namespace MovieRentalStore.API.Controllers
+namespace MovieRentalStore.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class MovieController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MovieController : ControllerBase
+    public MovieController() 
     {
+        
+    }
 
+    [HttpPost]
+    [SwaggerOperation(Summary = "Add new movie", Description = "Add new movie.")]
+    public IActionResult Add([FromBody]MovieInpuModel movie)
+    {
+        return Ok(movie);
+    }
+
+    [HttpPut]
+    [SwaggerOperation(Summary = "Update a movie", Description = "Update existing movie.")]
+    public IActionResult Update([FromBody] MovieInpuModel movie)
+    {
+        return Ok(movie);
+    }
+
+
+    [HttpGet]
+    [SwaggerOperation(Summary = "Get movies by title", Description = "Return all movies that contains the string in the title.")]
+    public IEnumerable<MovieInpuModel> GetListByTitle([FromQuery]string title)
+    {
+        MovieInpuModel movie = new() { Title = title };
+
+        List<MovieInpuModel> result = new() { movie };
+
+        return result;
+    }
+
+    [HttpDelete]
+    public IActionResult Delete([FromQuery]int id) 
+    {
+        return Delete(id);
     }
 }
---
+++ MovieRentalStore.API/DTOs/MovieInpuModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieRentalStore.API.DTOs;
+
+public class MovieInpuModel
+{
+    public string Title { get; set; }
+    public DateTime release { get; set; }
+    //TODO: Use Enum from Domain?
+    public IEnumerable<string> genre { get; set; }
+    public TimeSpan Runtime { get; set; }
+
+}
